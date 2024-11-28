d30cd7d001d5f272fe3e7277b264b726b2fd6d1f
AstorasDov <astorasdovgit@gmail.com>
2024-11-28 18:09:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("hello");
 
-fetch('https://astorasdov.github.io/authors.json');
+fetch('https://astorasdov.github.io/authors.json')
 .then((res)=>res.json())
 .then((data)=>console.log(data))
 console.log("hello");
